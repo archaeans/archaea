f55166eb945b00b659adc3eb5708e780b56e9d6e
oguzhankoral <oguzhankoral@gmail.com>
2022-12-26 13:18:02 +0300

    Create walls if not provided for zone

--- src/simulation_objects/zone.py
+++ src/simulation_objects/zone.py
@@ -8,20 +8,23 @@ class Zone:
     walls: "list[Border]"
     height: float
     volume: float
+    openings: "list[Border]"
 
-    def __init__(self, border, height):
+    def __init__(self, border, height, walls=None):
         self.floor = border
         self.height = height
         self.volume = self.floor.area * height
-        self.create_zone()
-
-    def create_zone(self):
-        wall_lines = self.floor.segments
         move_vector = Vector3d(0, 0, self.height)
+        self.ceiling = self.floor.move(move_vector)
+        if walls is None:
+            self.create_walls(move_vector)
+        else:
+            self.walls = walls
+
+    def create_walls(self, move_vector):
+        wall_lines = self.floor.segments
         walls = []
         for line in wall_lines:
             border = line.extrude(move_vector)
             walls.append(border)
         self.walls = walls
-        self.ceiling = self.floor.move(move_vector)
-
