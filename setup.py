d7460af616ecb98a6b90d585706140d4101588a9
oguzhankoral <oguzhankoral@gmail.com>
2023-01-23 22:47:28 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='archaea',
     packages=['archaea', 'archaea.geometry', 'archaea.earcut', 'archaea.writer'],
-    version='1.1.4',
+    version='1.1.5',
     license='Apache 2.0',
     description='Playground for Geometry!',
     readme='README.md',
     author='Oğuzhan Koral',
     author_email='oguzhankoral@gmail.com',
     url='https://github.com/archaeans/archaea',
-    download_url='https://github.com/archaeans/archaea/archive/refs/tags/1.1.4.tar.gz',
+    download_url='https://github.com/archaeans/archaea/archive/refs/tags/1.1.5.tar.gz',
     keywords=['geometry', 'mesh', 'stl', 'triangulation'],
     install_requires=[
         'numpy',
