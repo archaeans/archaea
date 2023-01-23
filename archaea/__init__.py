793dde3f34ff6be3090e630b03b628b51bf5e2a6
oguzhankoral <oguzhankoral@gmail.com>
2023-01-23 20:22:22 +0300

    Inculde earcut

--- archaea/__init__.py
+++ archaea/__init__.py
@@ -14,4 +14,5 @@ from archaea.geometry.utils import area, unit_normal
 from archaea.geometry.face import Face
 from archaea.geometry.mesh import Mesh
 from archaea.writer.to_stl import to_stl
+from archaea.earcut.earcut import *
 
