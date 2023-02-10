a3d3aeeeb27bdd6fcf4b9897da1457865fc91a55
oguzhankoral <oguzhankoral@gmail.com>
2023-02-10 21:34:03 +0300

    Disable orientation logic for now

--- archaea/geometry/loop.py
+++ archaea/geometry/loop.py
@@ -2,6 +2,7 @@ import functools
 from archaea.geometry.point3d import Point3d
 from archaea.geometry.point2d import Point2d
 from archaea.geometry.vector3d import Vector3d
+from archaea.geometry.vector2d import Vector2d
 from archaea.geometry.line_segment import LineSegment
 from archaea.geometry.polyline import Polyline
 from archaea.geometry.plane import Plane
@@ -35,14 +36,14 @@ class Loop(Polyline):
     def segment_count(self):
         return len(self.segments)
 
-    @functools.cached_property
-    def orientation(self) -> Orientation:
-        if self.area == 0:
-            return Orientation.UNDEFINED
-        elif self.area > 0:
-            return Orientation.COUNTER_CLOCKWISE
-        else:
-            return Orientation.CLOCKWISE
+    # @functools.cached_property
+    # def orientation(self) -> Orientation:
+    #     if self.area == 0:
+    #         return Orientation.UNDEFINED
+    #     elif self.area > 0:
+    #         return Orientation.COUNTER_CLOCKWISE
+    #     else:
+    #         return Orientation.CLOCKWISE
 
     def uv_points(self, plane=None) -> "list[Point2d]":
         plane = plane or self.plane()
@@ -80,8 +81,8 @@ class Loop(Polyline):
         return faces
 
     def offset(self, value):
-        if self.orientation == Orientation.UNDEFINED:
-            return self
+        # if self.orientation == Orientation.UNDEFINED:
+        #     return self
 
         loop_plane = self.plane()
         sc = self.segment_count
--- test/geometry/test_loop.py
+++ test/geometry/test_loop.py
@@ -12,8 +12,8 @@ class Setup(unittest.TestLoader):
     p3 = Point3d(10, 10, 0)
     p4 = Point3d(0, 10, 0)
     # counter-clockwise
-    loop_triangle = Loop([p1, p2, p3])
-    loop_rectangle = Loop([p1, p2, p3, p4])
+    counter_clockwise_loop_triangle = Loop([p1, p2, p3])
+    counter_clockwise_loop_rectangle = Loop([p1, p2, p3, p4])
     # clockwise
     clockwise_loop_triangle = Loop([p1, p3, p2])
     clockwise_loop_rectangle = Loop([p1, p4, p3, p2])
@@ -22,46 +22,49 @@ class Setup(unittest.TestLoader):
 class TestLoop(unittest.TestCase):
     def test_area(self):
         # Assert
-        self.assertEqual(Setup.loop_triangle.area, 50)
-        self.assertEqual(Setup.clockwise_loop_triangle.area, -50)
-        self.assertEqual(Setup.loop_rectangle.area, 100)
-        self.assertEqual(Setup.clockwise_loop_rectangle.area, -100)
+        self.assertEqual(Setup.counter_clockwise_loop_triangle.area, 50)
+        self.assertEqual(Setup.clockwise_loop_triangle.area, 50)
+        self.assertEqual(Setup.counter_clockwise_loop_rectangle.area, 100)
+        self.assertEqual(Setup.clockwise_loop_rectangle.area, 100)
 
-    def test_orientation(self):
-        # Assert
-        self.assertEqual(Setup.loop_rectangle.orientation, Orientation.COUNTER_CLOCKWISE)
-        self.assertEqual(Setup.clockwise_loop_rectangle.orientation, Orientation.CLOCKWISE)
+    # def test_clockwise_orientation(self):
+    #     # Assert
+    #     self.assertEqual(Setup.clockwise_loop_rectangle.orientation, Orientation.CLOCKWISE)
+
+    # def test_counter_clockwise_orientation(self):
+    #     # Assert
+    #     self.assertEqual(Setup.counter_clockwise_loop_rectangle.orientation, Orientation.COUNTER_CLOCKWISE)
 
     def test_close_loop(self):
         # Assert
-        self.assertEqual(len(Setup.loop_triangle.points), 4)
+        self.assertEqual(len(Setup.counter_clockwise_loop_triangle.points), 4)
 
     def test_calculate_normal(self):
         # Assert
-        self.assertEqual(Setup.loop_triangle.normal, Vector3d(0, 0, 1))
+        self.assertEqual(Setup.counter_clockwise_loop_triangle.normal, Vector3d(0, 0, 1))
 
     def test_points(self):
         # Assert
-        self.assertEqual(len(Setup.loop_rectangle.points), 5)
+        self.assertEqual(len(Setup.counter_clockwise_loop_rectangle.points), 5)
 
     def test_segments(self):
         # Act
-        segments = Setup.loop_rectangle.segments
+        segments = Setup.counter_clockwise_loop_rectangle.segments
 
         self.assertTrue(True)
 
     def test_segment_count(self):
         # Assert
-        self.assertEqual(Setup.loop_triangle.segment_count, 3)
+        self.assertEqual(Setup.counter_clockwise_loop_triangle.segment_count, 3)
 
     def test_extrude(self):
         # Act
-        faces = Setup.loop_rectangle.extrude(5)
+        faces = Setup.counter_clockwise_loop_rectangle.extrude(5)
         faces_area = sum(face.area for face in faces)
 
         # Assert
-        self.assertEqual(len(faces), 6)
-        self.assertEqual(faces_area, 400)
+        self.assertEqual(len(faces), 4)
+        self.assertEqual(faces_area, 200)
 
     def test_is_point_in_loop(self):
         # Arrange
@@ -72,10 +75,10 @@ class TestLoop(unittest.TestCase):
         point_in = Point3d(2, 2, 0)
 
         # Act
-        point_out_collinear_should_false = Setup.loop_rectangle.is_point_in_loop(point_out_collinear)
-        point_out_should_false = Setup.loop_rectangle.is_point_in_loop(point_out)
-        point_on_should_true = Setup.loop_rectangle.is_point_in_loop(point_on)
-        point_in_should_true = Setup.loop_rectangle.is_point_in_loop(point_in)
+        point_out_collinear_should_false = Setup.counter_clockwise_loop_rectangle.is_point_in_loop(point_out_collinear)
+        point_out_should_false = Setup.counter_clockwise_loop_rectangle.is_point_in_loop(point_out)
+        point_on_should_true = Setup.counter_clockwise_loop_rectangle.is_point_in_loop(point_on)
+        point_in_should_true = Setup.counter_clockwise_loop_rectangle.is_point_in_loop(point_in)
 
         # Assert
         self.assertFalse(point_out_collinear_should_false)
@@ -85,10 +88,10 @@ class TestLoop(unittest.TestCase):
 
     def test_uv_points(self):
         # Act
-        uv_points = Setup.loop_rectangle.uv_points()
+        uv_points = Setup.counter_clockwise_loop_rectangle.uv_points()
 
         # Assert
-        self.assertEqual(len(Setup.loop_rectangle.points), 5)
+        self.assertEqual(len(Setup.counter_clockwise_loop_rectangle.points), 5)
         self.assertEqual(len(uv_points), 4)
 
         self.assertEqual(uv_points[0], Point2d(0, 0))
@@ -96,14 +99,14 @@ class TestLoop(unittest.TestCase):
         self.assertEqual(uv_points[2], Point2d(10, -10))
         self.assertEqual(uv_points[3], Point2d(0, -10))
 
-        self.assertEqual(Setup.p1, Setup.loop_rectangle.point_from_uv(uv_points[0]))
-        self.assertEqual(Setup.p2, Setup.loop_rectangle.point_from_uv(uv_points[1]))
-        self.assertEqual(Setup.p3, Setup.loop_rectangle.point_from_uv(uv_points[2]))
-        self.assertEqual(Setup.p4, Setup.loop_rectangle.point_from_uv(uv_points[3]))
+        self.assertEqual(Setup.p1, Setup.counter_clockwise_loop_rectangle.point_from_uv(uv_points[0]))
+        self.assertEqual(Setup.p2, Setup.counter_clockwise_loop_rectangle.point_from_uv(uv_points[1]))
+        self.assertEqual(Setup.p3, Setup.counter_clockwise_loop_rectangle.point_from_uv(uv_points[2]))
+        self.assertEqual(Setup.p4, Setup.counter_clockwise_loop_rectangle.point_from_uv(uv_points[3]))
 
     def test_offset(self):
         # Act
-        offset_loop = Setup.loop_rectangle.offset(2)
+        offset_loop = Setup.counter_clockwise_loop_rectangle.offset(2)
 
         # Assert
         self.assertEqual(offset_loop.area, 36)
